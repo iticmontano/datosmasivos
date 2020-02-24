118684e731ecb08cf100600554768c28dcb5d98b
Raul Montaño <raulmontano97@gmail.com>
2020-02-24 01:05:12 -0800

    Examen Datos Masivos Unidad1 Opcion 2

--- Exam/Exam.scala
+++ Exam/Exam.scala
@@ -10,7 +10,14 @@ val diagonal_2: Int = (arr4._1._3) + (arr4._2._2) + (arr4._3._1) // res2: Int = 
 //de las diagonales y se agrega el ".abs" para calcular al absoluto
 def diagonalDefference(arr:Int): Int = {
     val arr = (diagonal_1 - diagonal_2).abs
-    return arr
+    arr
 }
 //para mostrar el resultado
 println(diagonalDefference(arr))
+
+//Opcion 2
+val arr4 = ((11, 2, 4), (4, 5, 6), (10, 8, -12))
+val diagonal_1: Int = (arr4._1._1) + (arr4._2._2) + (arr4._3._3)
+val diagonal_2: Int = (arr4._1._3) + (arr4._2._2) + (arr4._3._1)
+def diagonalDefference(a:Int, b:Int): Int = (a - b).abs
+diagonalDefference(diagonal_1,diagonal_2)
