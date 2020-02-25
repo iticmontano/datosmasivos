5aaa84a65b1f2eb37a17963a7ee0950dcfbb2c67
pepebull <60455910+pepebull@users.noreply.github.com>
2020-02-24 18:38:09 -0800

    Update P1.scala
    English commit

--- Practices/P1.scala
+++ Practices/P1.scala
@@ -1,11 +1,11 @@
 // Assessment 1/Practica 1
-//1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
-    //Declarar la operacion de 2(pi) dentro de un value "radio"
+//1. Develop an algorithm in scala that calculates the radius of a circle
+    // Declare the operation of 2 (pi) within a "radius" value
         val radio:Int = 2*3.1415926535
     //realizar una operacion
         16/radio //res0: 2.568991
 
-//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+//2. Develop an algorithm in scala that tells me if a number is a cousin
     def isPrime(num:Int): Boolean = {
     for(n <- Range(2, num)){
         if(num%n == 0){
@@ -15,18 +15,18 @@
     return true
     }
     println(isPrime(10))
-//3. Dada la variable bird = "tweet", utiliza interpolacion de string para
-//   imprimir "Estoy ecribiendo un tweet"
+//3. Given the variable bird = "tweet", use string interpolation to
+// print "I am writing a tweet"
     val bird = "tweet"
     println(s"Estoy escribiendo un $bird")
 
-//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
-//   secuencia "Luke"
-    var mensaje = "Hola Luke yo soy tu padre!"
-    mensaje slice(5,9)
+//4. Given the variable message = "Hi Luke, I'm your father!" use slilce to extract the
+// sequence "Luke"
+    var message = "Hi Luke, I'm your father!"
+    message slice(3,7)
 
-//5. Cual es la diferencia en value y una variable en scala?
-    el valie es inmutable y la variable puede cambiar de valor
-//6. Dada la tupla ((2,4,5),(1,2,3),(3.1416,23)) regresa el numero 3.1416 
+//5. What is the difference in value and a variable in scala?
+    the valie is immutable and the variable can change in value
+// 6. Given the tuple ((2,4,5), (1,2,3), (3,114,23)), return the number 3.1416
     val tup = ((2,4,5),(1,2,3),(3.1416,23))
     println(tup._3._1)
