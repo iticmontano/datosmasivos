8996e87cf95b6227431004db835c1db24e7da3ec
Raul Montaño <raulmontano97@gmail.com>
2020-02-24 18:07:44 -0800

    Terminado

--- Practices/P2.scala
+++ Practices/P2.scala
@@ -11,7 +11,8 @@
     Array.range(1, 1000, 5)
 
 // 5. Cuales son los elementos unicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversion a conjuntos
-
+    val lista = List(1,3,3,4,6,7,3,7)
+    lista.toSet
 // 6. Crea una mapa mutable llamado nombres que contenga los siguiente
 //     "Jose", 20, "Luis", 24, "Ana", 23, "Susana", "27"
     val nombres = collection.mutable.Map(("Jose", 20), ("Luis", 24), ("Ana", 23), ("Susana", 27))
