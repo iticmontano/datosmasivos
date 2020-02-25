00df4216f084bb119e57b58b803155ad38b6d7fb
Raul Montaño <45322152+iticmontano@users.noreply.github.com>
2020-02-24 18:41:36 -0800

    English version

--- Exam/Exam.scala
+++ Exam/Exam.scala
@@ -1,10 +1,10 @@
-//Opcion 2
-//Declarar la Variable con la matriz
+//Masive Data Exam
+//Declare variable with the matrix
 val arr4 = ((11, 2, 4), (4, 5, 6), (10, 8, -12))
-//Declaras variables con ls operaciones de las diagonales
+//Declare variable with the operations of the both diagonals
 val diagonal_1: Int = (arr4._1._1) + (arr4._2._2) + (arr4._3._3)
 val diagonal_2: Int = (arr4._1._3) + (arr4._2._2) + (arr4._3._1)
-//funcion para determinar el valor absoluto de las diagonales
+//function to calculate the absolut value
 def diagonalDefference(a:Int, b:Int): Int = (a - b).abs
-//llamar la fincion con la operacion
+//call the function
 diagonalDefference(diagonal_1,diagonal_2)
